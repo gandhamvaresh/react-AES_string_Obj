c1717a4ad6a42548eda5c723fa95c11a22f55dcb
gandhamvaresh <gandhamvaresh@gmail.com>
2022-02-26 19:38:16 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -175,6 +175,8 @@ export default function App() {
         </div>
       </form>
       <div>
+        <label> Output:</label>
+        <br />
         <p> {outPutValue} </p>{' '}
       </div>
     </div>
